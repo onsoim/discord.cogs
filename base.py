0b7c86aedaaab172e89e25cc7ae710deb17b1bea
onsoim <onsoim@gmail.com>
2022-12-01 01:41:02 +0900

    [ADD] ping command as base command

--- base.py
+++ base.py
@@ -1,16 +1,24 @@
 
-from discord.ext.commands   import Cog
+from discord.ext.commands   import Cog, command
 
 
 class Base(Cog):
+    prefixes = ['/', '!', '.', '?']
+
     def __init__(self, bot):
-        self.bot = bot
         # print('init base cog')
+        self.bot = bot
 
     @Cog.listener()
     async def on_message(self, message):
-        print(f'{message.author} (cogs.base)\n=> {message.content}\n')
-        await self.bot.process_commands(message)
+        await print(f'{message.author} (cogs.base)\n=> {message.content}\n')
+    #     await self.bot.process_commands(message)
+
+    @command(
+        aliases = [ f'{prefix}ping' for prefix in prefixes ]
+    )
+    async def ping(self, ctx):
+        await ctx.send(f'pong! {round(round(self.bot.latency, 4) * 1000)}ms')
 
 
 async def setup(bot):

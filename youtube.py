5bb1eb867ee9e570abe9ace79607fe3238ca1855
onsoim <onsoim@gmail.com>
2022-12-02 14:43:44 +0900

    [ADD] reply with video file if file is not exceed 8MB

--- youtube.py
+++ youtube.py
@@ -1,23 +1,39 @@
 
+from discord                import File
 from discord.ext.commands   import Cog
+from pytube                 import YouTube  as YT
 
+import os
 import re
 
 
 class YouTube(Cog):
     def __init__(self, bot):
-        print('init youtube cog')
+        # print('init youtube cog')
         self.bot    = bot
         self.re     = {
-            'url': '(?:https?:\\/\\/)?(?:m\\.|www\\.)?(?:youtu\\.be\\/|youtube\\.com\\/(?:embed\\/|v\\/|watch\\?v=|watch\\?.+&v=))((\\w|-){11})(\\?\\S*)?'
+            'url': r'((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?'
         }
+        self.data   = './data'
+        os.makedirs(self.data, exist_ok=True)
 
     @Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user: return
 
         for url in re.finditer(self.re['url'], message.content):
-            await message.reply(url[0])
+            yt = YT(url[0])
+
+            filename = f'{self.data}/{yt.video_id}.mp4'
+            yt.streams.get_highest_resolution().download(self.data, f'{yt.video_id}.mp4')
+
+            # await message.reply(yt.video_id if os.path.getsize(filename) < 8388608 else "File size exceeds 8MB.")
+            if os.path.getsize(filename) < 8388608:
+                await message.channel.send(
+                    yt.title,
+                    file = File(filename),
+                    reference = message
+                )
 
 
 async def setup(bot):
